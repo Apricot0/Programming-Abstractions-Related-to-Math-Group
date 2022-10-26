e3484c213236c4aee73d9cb3007e879d05bba344
jasper <zqqyoung@gmail.com>
2022-10-26 00:04:11 -0400

    finish -1 or 1

---
+++ src/arithmetic/FiniteGroupOfOrderTwo.java
@@ -0,0 +1,48 @@
+package arithmetic;
+
+import core.Group;
+public class FiniteGroupOfOrderTwo implements Group<PlusOrMinusOne>{
+    /**
+     *
+     * @param one   the object that is the first argument of the binary operation.
+     * @param other the other object (the second argument of the binary operation) to be combined with this object as
+     *              per the group's binary operation.
+     * @return one * other
+     */
+    @Override
+    public PlusOrMinusOne binaryOperation(PlusOrMinusOne one, PlusOrMinusOne other) {
+        return new PlusOrMinusOne(one.getValue()* other.getValue());
+    }
+
+    /**
+     * 1 times -1 or 1 is still -1 or 1, so 1 is the identity
+     * @return 1
+     */
+    @Override
+    public PlusOrMinusOne identity() {
+        return new PlusOrMinusOne(1);
+    }
+
+    /**
+     * -1*-1= 1; 1*1 = 1, so -1 or 1 's inverses are themselves
+     * @param plusOrMinusOne
+     * @return the inverse
+     */
+    @Override
+    public PlusOrMinusOne inverseOf(PlusOrMinusOne plusOrMinusOne) {
+        return plusOrMinusOne;
+    }
+
+    /**
+     *
+     * @param plusOrMinusOne the group element serving as the base.
+     * @param k the integer exponent, indicating the number of times the binary operation is applied on <code>t</code>.
+     *          The exponent must be a non-negative integer value.
+     * @return the result after do binary operation on -1 or 1 k times
+     */
+    @Override
+    public PlusOrMinusOne exponent(PlusOrMinusOne plusOrMinusOne, int k) {
+        return Group.super.exponent(plusOrMinusOne, k);
+    }
+}
+
---
+++ src/arithmetic/PlusOrMinusOne.java
@@ -0,0 +1,22 @@
+package arithmetic;
+
+public class PlusOrMinusOne {
+    private int value;
+    public PlusOrMinusOne(int x){
+        if ( !(x == -1 || x == 1)) throw new IllegalArgumentException();
+        value = x;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    static public PlusOrMinusOne[] values(){
+        PlusOrMinusOne[] set = new PlusOrMinusOne[2];
+        PlusOrMinusOne one = new PlusOrMinusOne(1);
+        PlusOrMinusOne neg_one = new PlusOrMinusOne(-1);
+        set[0]=one;
+        set[1]=neg_one;
+        return set;
+    }
+}
