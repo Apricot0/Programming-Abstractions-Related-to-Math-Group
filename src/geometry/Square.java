1902420fdd98a713de23fae55d9f46ce6de49946
jasper <zqqyoung@gmail.com>
2022-10-29 15:23:43 -0400

    Square update

--- src/geometry/Square.java
+++ src/geometry/Square.java
@@ -1,5 +1,9 @@
 package geometry;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Square extends Shape {
 
     Point center;
@@ -7,24 +11,59 @@ public class Square extends Shape {
     Point downLeft;
     Point upLeft;
     Point upRight;
+
+    List<Point> squarePoints;
     @Override
     public Point center() {
-        return null; // TODO: part of the assignment
+        return center;
     }
 
     @Override
     public Square rotateBy(int degrees) {
-        return null; // TODO: part of the assignment
+        double newDegrees = Math.toRadians(degrees);
+        if (squarePoints==null){
+            throw new RuntimeException("Invalid Square");
+        }
+
+        List<Point> newPoints = new ArrayList<Point>();
+        for (Point p: squarePoints){
+            double newX, newY;
+            newX =((p.x-center.x)*Math.cos(newDegrees)-(p.y-center.y)*Math.sin(newDegrees))+center.x;
+            newY =((p.x-center.x)*Math.sin(newDegrees)+(p.y-center.y)*Math.cos(newDegrees))+center.y;
+            newX = (double)Math.round((newX*10)/10);
+            newY = (double)Math.round((newY*10)/10);
+            newPoints.add(new Point(p.name,newX,newY));
+        }
+        return new Square(newPoints.get(0),newPoints.get(1),newPoints.get(2),newPoints.get(3));
     }
 
     @Override
     public Shape translateBy(double x, double y) {
-        return null; // TODO: part of the assignment
+        if (squarePoints==null){
+            throw new RuntimeException("Invalid Square");
+        }
+
+        List<Point> newPoints = new ArrayList<Point>();
+        for (Point p: squarePoints){
+            double newX, newY;
+            newX = p.x + x;
+            newY = p.y + y;
+            newPoints.add(new Point(p.name,newX,newY));
+        }
+        return new Square(newPoints.get(0),newPoints.get(1),newPoints.get(2),newPoints.get(3));
     }
 
     @Override
     public String toString() {
-        return null; // TODO: part of the assignment
+        String output = "[";
+        if(squarePoints!=null) {
+            for (int i = 0; i < squarePoints.size(); i++) {
+                output += "; ";
+                output += squarePoints.get(i).toString();
+            }
+        }
+        output+="]";
+        return output;
     }
 
     public Square(Point a, Point b, Point c, Point d) {
@@ -32,10 +71,16 @@ public class Square extends Shape {
         upLeft=b;
         downLeft=c;
         downRight=d;
+        squarePoints = Arrays.asList(upRight,upLeft,downLeft,downRight);
         if (!(Distance(a,b)==Distance(b,c) && Distance(b,c) ==Distance(c,d) && Distance(c,d) == Distance(d,a) && Distance(d,a)==Distance(a,d))){
             throw new IllegalArgumentException ("Not every edges are equal!");
         }
-
+        if ( a.equals(b)||b.equals(c)||c.equals(d)||d.equals(a)){
+            throw new IllegalArgumentException("Can not have two same points!");
+        }
+        if (!verify90(a,b,d)){
+            throw new IllegalArgumentException("Please make sure every angle is 90 degree");
+        }
         double xOfCenter = (downLeft.x+upRight.x)/2;
         double yofCenter = (downLeft.y+upRight.y)/2;
         center = new Point ("center",xOfCenter,yofCenter);
@@ -43,25 +88,12 @@ public class Square extends Shape {
     private static double Distance (Point a, Point b){
         return Math.sqrt( (a.x-b.x)*(a.x-b.x) + (a.y-b.y)*(a.y-b.y));
     }
-    private static double TiltAngle (Point a, Point b) {
-        double xDiff = b.x-a.x;
-        double yDiff = b.y-a.y;
-        double tanVal = yDiff/xDiff;
-        double degree = Math.toDegrees(Math.atan(tanVal));
-        if (xDiff>=0){
-            if (yDiff>=0){
-                return degree;
-            }else{
-                return 360-degree;
-            }
-        }else{
-            if (yDiff>=0){
-                return 180 - degree;
-            }else{
-                return 360 + degree;
-            }
-        }
 
+    private static boolean verify90(Point a, Point b, Point c){
+        //move sub graph a b c to b = (0,0) so:
+        Point newA = new Point (a.name, a.x-b.x, a.y-b.y);
+        Point newC = new Point (c.name, c.x-b.x, c.y-b.y);
+        return (newA.y==-newC.x && newA.x == newC.y);
     }
 
 
